9c94f156664c7d7620db2b65bfa4fad392d6a248
Michael Janschek <michael.janschek@hhu.de>
2012-04-30 17:23:47 +0200

    eingerückt

--- src/Wuerfel.java
+++ src/Wuerfel.java
@@ -1,8 +1,7 @@
-
 public class Wuerfel {
-	
-	public static void main(String[] args){
-		int w = (int) (Math.random()*6)+1;
+
+	public static void main(String[] args) {
+		int w = (int) (Math.random() * 6) + 1;
 		System.out.println(w);
 	}
 
