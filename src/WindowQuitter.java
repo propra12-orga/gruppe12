ad41defa7cccfd6c22fa1f889707492f8bae140f
gargi <michael.janschek@hhu.de>
2012-05-09 19:09:38 +0200

    Struktur ueberarbeitet und uebersichtlicher gestaltelt, packages definiert

--- src/WindowQuitter.java
+++
@@ -1,9 +0,0 @@
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-public class WindowQuitter extends WindowAdapter // WindowListener
-{
-	public void windowClosing(WindowEvent e) {
-		System.exit(0); // Beendet Programm
-	}
-}
