bb418aee4b9cb9776bb1749cb58c1eac3e2bdd6f
unknown <Benjamin.Rose@uni-duesseldorf.de>
2012-05-10 16:44:38 +0200

    Erstellung der Klasse Objekte
    Implementierung der Klasse Objekte und deren Variabeln, Konstruktoren,
    Methoden, sowie Set-und Get-Methoden.

--- src/spielfeld/Objekte.java
+++ src/spielfeld/Objekte.java
@@ -1,19 +1,89 @@
 package spielfeld;
 
-public interface Objekte {
+public class Objekte {
+  
 
-	/*
-	 * Set und Get Methoden sind fuer die Kollisionsabfrage und Settings gut
-	 * geeignet
-	 */
-	/* das muss ein Objekt können */
+  //Dekleration von Variabeln
+  protected int      xPosition;
+  protected int      yPosition;
+  protected int      width;
+  protected int      height;
+  protected String   pic;
+  
+  protected String   type;
+  
+  // Konstruktor
+  
+  public Objekte(int xPos ,int yPos,int widht, int height, String pic, String type)
+  {
+    xPosition        = xPos;
+    yPosition        = yPos;
+    this.width       = widht;
+    this.height      = height;
+    this.pic         = pic ;
+    this.type        = type ;
+  }
+  
+  //Methode zum bewegen von Objekten. Muss überschrieben werden
+  public void move(){
+  }
+  
+  //Löschen von Objekten
+  public void kill(){
+  }
+  
+  
+  // Set- und Get-Methoden
+  
+  //Set:
+  
+  public void setXPosition(int pos)
+  {
+    xPosition = pos;
+  }
+  public void setYPosition(int pos)
+  {
+    yPosition = pos;
+  }
+  public void setWidth(int width)
+  {
+    this.width = width;
+  }
+  public void setHeight(int height)
+  {
+    this.height = height;
+  }
+  
 
-	public int getX();
 
-	public int getY();
+  //Get:
+  public double getXPosition()
+  {
+    return xPosition;
+  }
+  public double getYPosition()
+  {
+    return yPosition;
+  }
+  public int getWidth()
+  {
+    return width;
+  }
+  public int getHeight()
+  {
+    return height;
+  }
+  public String getPic()
+  {
+    return pic;
+  }
+  public String getType()
+  {
+    return type;
+  }
 
-	public void setX(int x);
 
-	public void setY(int y);
-
+  
+  
+  
 }
