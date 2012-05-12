448e8ef6bc6073de9e1c3563d6a561c280a0be66
Wayengar <Markus@Markus-PC.(none)>
2012-05-12 16:39:18 +0200

    Update Spielfigur

--- src/spielfigur/Spielfigur.java
+++ src/spielfigur/Spielfigur.java
@@ -7,8 +7,32 @@ import spielfeld.Bombe;
 
 public class Spielfigur implements KeyListener {
 
+	private int xpos;
+	private int ypos;
+
 	public Spielfigur() {
 		addKeyListener(this);
+	}
+
+	public Spielfigur(int xpos, int ypos) {
+		this.ypos = ypos;
+		this.xpos = xpos;
+	}
+
+	private int getxPosition() {
+		return xpos;
+	}
+
+	private void setxPosition(int xpos) {
+		this.xpos = xpos;
+	}
+
+	private int getyPosition() {
+		return ypos;
+	}
+
+	private void setyPosition(int ypos) {
+		this.xpos = ypos;
 	}
 
 	public void bombeLegen() {
@@ -22,12 +46,30 @@ public class Spielfigur implements KeyListener {
 	}
 
 	public void keyTyped(KeyEvent e) {
+
 	}
 
 	public void keyReleased(KeyEvent e) {
 	}
 
 	public void keyPressed(KeyEvent e) {
+
+		if (key.getKeyCode() == Key.VK_UP) {
+
+		}
+
+		if (Key.getKeyCode() == Key.VK_LEFT) {
+
+		}
+
+		if (Key.getKeyCode() == Key.VK_RIGHT) {
+			;
+		}
+
+		if (Key.getKeyCode() == Key.VK_DOWN) {
+
+		}
+
 	}
 
 }
