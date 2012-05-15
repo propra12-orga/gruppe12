757a5f2327f81de6c9a760985d6c80038c549d64
Tony <tony@tony-Aspire-7750G.(none)>
2012-05-15 09:50:52 +0200

    Dummy thread fuer spielfeld, dynamische vergoeßerung spielfeld in spielflaeche

--- src/game/TestRun.java
+++ src/game/TestRun.java
@@ -4,22 +4,24 @@ import javax.swing.JFrame;
 
 import menu.MainMenu;
 
-import spielfeld.Spielflaeche;
-import tools.GameQuitter;
+
 
 public class TestRun {
 	public static void go() {
-		JFrame opener = new JFrame("Graphicstester");
-		GameQuitter gQuit = new GameQuitter(opener); // Erzeugt WindowListener
+		
+		
 														// vom Typ GameQuitter
+		Dummy a=new Dummy();	 // erstellt das Spielfeld
+		
+		
+		try{
+		Thread.sleep(1000);}  // wartet explizit auf a.start() welches die Anzeige eroeffnet
+		
+		catch(InterruptedException e){}
+		
+		a.start();
+		
 
-		opener.setLocation(400, 25);
-		opener.setSize(822, 849);
-
-		opener.addWindowListener(gQuit); // gQuit wird hinzugefügt.
-
-		opener.add(new Spielflaeche());
-		opener.setVisible(true);
 		MainMenu.gamerunning = false; // teilt StartMenu mit, dass das Spiel
 										// läuft.
 	}
