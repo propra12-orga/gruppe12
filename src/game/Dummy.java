1f7e5252ad7a0208954bc674596eaf2831888cb6
gargi <michael.janschek@hhu.de>
2012-05-15 13:14:44 +0200

    Anpassung vom letzten Push.

--- src/game/Dummy.java
+++ src/game/Dummy.java
@@ -1,39 +1,32 @@
+package game;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+
 import spielfeld.Spielflaeche;
 import tools.GameQuitter;
-// Diese Klasse erzeugt ein Spielfeld und benutzt einen Thread zum darstellen
-// wahrscheinlich fixt das das Problem, es ist bei mir nichtmehr aufgetreten
 
 public class Dummy extends Thread {
-	public Dummy(){   // Konstruktor erzeugt Frame mit Spielfeld
+	public Dummy() { // Konstruktor erzeugt Frame mit Spielfeld
 		final JFrame opener = new JFrame("Graphicstester");
-		GameQuitter gQuit = new GameQuitter(opener); // Erzeugt WindowListener		
+		GameQuitter gQuit = new GameQuitter(opener); // Erzeugt WindowListener
 		opener.add(new Spielflaeche());
 		opener.setLocation(400, 25);
-		opener.setSize(800,600);
+		opener.setSize(800, 600);
 		opener.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		opener.addWindowListener(gQuit);
-		
-		
-		
-		
-		Runnable gui=new Runnable(){
+
+		Runnable gui = new Runnable() {
 
 			@Override
-			public void run() {			  // der Thread zeichnet durchgehend neu 
+			public void run() { // der Thread zeichnet durchgehend neu
 				opener.repaint();
 				opener.setVisible(true);
 			}
-			
+
 		};
 		SwingUtilities.invokeLater(gui); // wartet bis ausgefuehrt
-			
-		
-		
-		
-		
-		
+
 	}
 
 }
--- src/game/TestRun.java
+++ src/game/TestRun.java
@@ -1,29 +1,21 @@
 package game;
 
-import javax.swing.JFrame;
-
-import menu.MainMenu;
-
-
 
 public class TestRun {
 	public static void go() {
-		
-		
-														// vom Typ GameQuitter
-		Dummy a=new Dummy();	 // erstellt das Spielfeld
-		
-		
-		try{
-		Thread.sleep(1000);}  // wartet explizit auf a.start() welches die Anzeige eroeffnet
-		
-		catch(InterruptedException e){}
-		
-		a.start();
-		
 
-		MainMenu.gamerunning = false; // teilt StartMenu mit, dass das Spiel
-										// läuft.
+		Dummy a = new Dummy(); // erstellt das Spielfeld
+		a.run();
+
+		try {
+			Thread.sleep(1000);
+		} // wartet explizit auf a.start() welches die Anzeige eroeffnet
+
+		catch (InterruptedException e) {
+
+		}
+
+		a.start();
 	}
 
 }
