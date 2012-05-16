5089b2e16ef5bf4c6fecf9064f6bb42c0ce4fc47
Tony <tony@tony-Aspire-7750G.(none)>
2012-05-16 19:42:17 +0200

    Spielfeldklasse uebersichtlicher, methoden hinzugefuegt

--- src/spielfeld/Spielfeld.java
+++ src/spielfeld/Spielfeld.java
@@ -1,6 +1,12 @@
 package spielfeld;
 
-public class Spielfeld {
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JPanel;
+
+public class Spielfeld extends JPanel {
 
 	// Register- Zahlen:
 	/*
@@ -12,6 +18,28 @@ public class Spielfeld {
 
 	public Spielfeld(int Breite, int Hoehe) { // initialisiert Register
 		Register = new int[Breite][Hoehe];
+		for (int i = 0; i < Breite; i++) {
+			for (int j = 0; j < Hoehe; j++) {
+				Register[i][j] = 0;
+			}
+		}
+
+	}
+
+	public void Randfuellen() {
+		for (int y = 0; y < 20; y++) {
+			Register[0][y] = 2;
+		} // Raender mit Mauer befuellen
+		for (int y = 0; y < 20; y++) {
+			Register[19][y] = 2;
+		}
+		for (int x = 0; x < 20; x++) {
+			Register[x][0] = 2;
+		}
+		for (int x = 0; x < 20; x++) {
+			Register[x][19] = 2;
+		}
+
 	}
 
 	public void fill(int RegX, int RegY, int Obj) { // Methode zum befuellen des
@@ -23,6 +51,41 @@ public class Spielfeld {
 	public int getObj(int RegX, int RegY) { // gibt das Objekt an der Stelle
 											// RegX,RegY aus dem Register wieder
 		return Register[RegX][RegY];
+	}
+
+	public void draw(Image img, int x, int y, Graphics g) {
+
+		g.drawImage(img, x * (getWidth() / 20), y * (getHeight() / 20),
+				getWidth() / 20 + 19, (getHeight() / 20) + 19, null);
+	}
+
+	public Image loadImg(String Pfad) {
+		Image Name1 = Toolkit.getDefaultToolkit().getImage(
+				getClass().getResource(Pfad));
+
+		return Name1;
+
+	}
+
+	public boolean equalsGras(int x, int y) {
+		if (Register[x][y] == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean equalsMauer(int x, int y) {
+		if (Register[x][y] == 2)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean equalsExit(int x, int y) {
+		if (Register[x][y] == 1)
+			return true;
+		else
+			return false;
 	}
 
 	/* Rest noch zu editieren */
--- src/spielfeld/Spielflaeche.java
+++ src/spielfeld/Spielflaeche.java
@@ -2,7 +2,6 @@ package spielfeld;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
 
 import javax.swing.JPanel;
 
@@ -16,66 +15,37 @@ public class Spielflaeche extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	public void paint(Graphics g) {
-
-		// auslesen einer Bilddatei, wichtig: vorher muss sie in Source Code in
-		// Eclipse importiert werden!
-		// src > import...>file system>pfad suchen > Datei importieren
-		// super.paint(g);
-		Image gras = Toolkit.getDefaultToolkit().getImage(
-				getClass().getResource("/ressources/grafics/gras.jpg"));
-		Image mauer = Toolkit.getDefaultToolkit().getImage(
-				getClass().getResource("/ressources/grafics/brick.jpg"));
-		Image exit = Toolkit.getDefaultToolkit().getImage(
-				getClass().getResource("/ressources/grafics/ausgang.jpg"));
-
 		play = new Spielfeld(20, 20); // intern wird das spielfeld als
-										// array implementiert
+										// array implementiert und direkt
+										// befuellt mit 0
 
-		for (int x = 0; x < 20; x++) { // befuellt das Array mit einem neutralen
-										// Element 0, hier: Gras
-			for (int y = 0; y < 20; y++) {
-				play.fill(x, y, 0);
-			}
-		}
-		for (int y = 0; y < 20; y++) {
-			play.fill(0, y, 2);
-		} // Raender mit Mauer befuellen
-		for (int y = 0; y < 20; y++) {
-			play.fill(19, y, 2);
-		}
-		for (int x = 0; x < 20; x++) {
-			play.fill(x, 0, 2);
-		}
-		for (int x = 0; x < 20; x++) {
-			play.fill(x, 19, 2);
-		}
-		play.fill(18, 18, 1);
+		Image gras = play.loadImg("/ressources/grafics/gras.jpg");
+		Image mauer = play.loadImg("/ressources/grafics/brick.jpg");
+		Image exit = play.loadImg("/ressources/grafics/ausgang.jpg");
+
+		play.Randfuellen(); // Befuellt die Raender mit Mauer
+		play.fill(18, 18, 1); // Ausgang
 
 		for (int x = 0; x < 20; x++) {
 			for (int y = 0; y < 20; y++) {
-				// wichtig: Register Zahlen befinden sich in der Klasse
-				// Spielfeld.java
+				int a = getWidth() / 20;
+				int b = getHeight() / 20;
 
-				if (play.getObj(x, y) == 0) { // zeichnet die Objekte 1
-					g.drawImage(gras, x * (getWidth() / 20), y
-							* (getHeight() / 20), getWidth() / 20 + 1,
-							(getHeight() / 20) + 1, null); // hier wird das bild
-															// "gras.jpg"
-															// gezeichnet, also
-															// Grasflaeche
+				if (play.equalsGras(x, y)) {
+
+					g.drawImage(gras, x * a, y * b, a + 1, b + 1, null); // zeichnet
+																			// Gras
 
 				}
-				if (play.getObj(x, y) == 2) {
-					g.drawImage(mauer, x * (getWidth() / 20), y
-							* (getHeight() / 20), getWidth() / 20 + 19,
-							(getHeight() / 20) + 19, null);
-				} // +19 um die Groeße an Frame anzupassen, /20 weil es 20
-					// Bilder sind die sich den Frame teilen
+				if (play.equalsMauer(x, y)) {
 
-				if (play.getObj(x, y) == 1) {
-					g.drawImage(exit, x * (getWidth() / 20), y
-							* (getHeight() / 20), getWidth() / 20 + 19,
-							(getHeight() / 20) + 19, null);
+					g.drawImage(mauer, x * a, y * b, a + 19, b + 19, null); // zeichnet
+																			// Mauer
+
+				}
+				if (play.equalsExit(x, y)) {
+					g.drawImage(exit, x * a, y * b, a + 19, b + 19, null); // zeichnet
+																			// Ausgang
 				}
 			}
 		}
